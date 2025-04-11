21bb03a5b63983285ff80833906208288e55c064
TomW <tomw7@orange.fr>
2025-04-11 12:54:22 +0100

    Push 2

--- VAE.py
+++ VAE.py
@@ -1,71 +1,223 @@
-import torch
-import numpy as np
-import torch.nn as nn
-import torch.functional as F
-import torch.transform as T
-from PIL import Image 
-import os
-
-
-def data_loader():
-    train_x, train_y = [], []
-    test_x, test_y = [], []
-
-    data_dir = os.path.join(os.getcwd(), 'EM_Data', 'original')
-
-    data_sets = [f for f in os.listdir(data_dir) if not f.startswith('.')]
-
-    for data_set in data_sets:
-        dataset_path = os.path.join(data_dir, data_set)
-        
-        if data_set == 'train':
-            for coordinate in os.listdir(dataset_path):
-                coord_path = os.path.join(dataset_path, coordinate)
-                if not os.path.isdir(coord_path):
-                    continue
-                    
-                if coordinate == 'x':
-                    for image in os.listdir(coord_path):
-                        if image.startswith('.'):
-                            continue
-                        img_path = os.path.join(coord_path, image)
-                        with Image.open(img_path) as img:
-                            train_x.append(np.array(img))
-                else:
-                    for image in os.listdir(coord_path):
-                        if image.startswith('.'):
-                            continue
-                        img_path = os.path.join(coord_path, image)
-                        with Image.open(img_path) as img:
-                            train_y.append(np.array(img))
-        else:
-            for coordinate in os.listdir(dataset_path):
-                coord_path = os.path.join(dataset_path, coordinate)
-                if not os.path.isdir(coord_path):
-                    continue
-                    
-                if coordinate == 'x':
-                    for image in os.listdir(coord_path):
-                        if image.startswith('.'):
-                            continue
-                        img_path = os.path.join(coord_path, image)
-                        with Image.open(img_path) as img:
-                            test_x.append(np.array(img))
-                else:
-                    for image in os.listdir(coord_path):
-                        if image.startswith('.'):
-                            continue
-                        img_path = os.path.join(coord_path, image)
-                        with Image.open(img_path) as img:
-                            test_y.append(np.array(img))
-
-    return np.array(train_x), np.array(train_y), np.array(test_x), np.array(test_y)
-
-
+from common import *
 
 class VAE(nn.Module):
     def __init__(self):
         super().__init__()
         
         self.FC1 = nn.Linear()
-        
+
+import torch
+from models import BaseVAE
+from torch import nn
+from torch.nn import functional as F
+from .types_ import *
+
+
+class VanillaVAE(BaseVAE):
+
+
+    def __init__(self,
+                 in_channels: int,
+                 latent_dim: int,
+                 hidden_dims: List = None,
+                 **kwargs) -> None:
+        super(VanillaVAE, self).__init__()
+
+        self.latent_dim = latent_dim
+
+        modules = []
+        if hidden_dims is None:
+            hidden_dims = [32, 64, 128, 256, 512]
+
+        # Build Encoder
+        for h_dim in hidden_dims:
+            modules.append(
+                nn.Sequential(
+                    nn.Conv2d(in_channels, out_channels=h_dim,
+                              kernel_size= 3, stride= 2, padding  = 1),
+                    nn.BatchNorm2d(h_dim),
+                    nn.LeakyReLU())
+            )
+            in_channels = h_dim
+
+        self.encoder = nn.Sequential(*modules)
+        self.fc_mu = nn.Linear(hidden_dims[-1]*4, latent_dim)
+        self.fc_var = nn.Linear(hidden_dims[-1]*4, latent_dim)
+
+
+        # Build Decoder
+        modules = []
+
+        self.decoder_input = nn.Linear(latent_dim, hidden_dims[-1] * 4)
+
+        hidden_dims.reverse()
+
+        for i in range(len(hidden_dims) - 1):
+            modules.append(
+                nn.Sequential(
+                    nn.ConvTranspose2d(hidden_dims[i],
+                                       hidden_dims[i + 1],
+                                       kernel_size=3,
+                                       stride = 2,
+                                       padding=1,
+                                       output_padding=1),
+                    nn.BatchNorm2d(hidden_dims[i + 1]),
+                    nn.LeakyReLU())
+            )
+
+
+
+        self.decoder = nn.Sequential(*modules)
+
+        self.final_layer = nn.Sequential(
+                            nn.ConvTranspose2d(hidden_dims[-1],
+                                               hidden_dims[-1],
+                                               kernel_size=3,
+                                               stride=2,
+                                               padding=1,
+                                               output_padding=1),
+                            nn.BatchNorm2d(hidden_dims[-1]),
+                            nn.LeakyReLU(),
+                            nn.Conv2d(hidden_dims[-1], out_channels= 3,
+                                      kernel_size= 3, padding= 1),
+                            nn.Tanh())
+
+    def encode(self, input: Tensor) -> List[Tensor]:
+        """
+        Encodes the input by passing through the encoder network
+        and returns the latent codes.
+        :param input: (Tensor) Input tensor to encoder [N x C x H x W]
+        :return: (Tensor) List of latent codes
+        """
+        result = self.encoder(input)
+        result = torch.flatten(result, start_dim=1)
+
+        # Split the result into mu and var components
+        # of the latent Gaussian distribution
+        mu = self.fc_mu(result)
+        log_var = self.fc_var(result)
+
+        return [mu, log_var]
+
+    def decode(self, z: Tensor) -> Tensor:
+        """
+        Maps the given latent codes
+        onto the image space.
+        :param z: (Tensor) [B x D]
+        :return: (Tensor) [B x C x H x W]
+        """
+        result = self.decoder_input(z)
+        result = result.view(-1, 512, 2, 2)
+        result = self.decoder(result)
+        result = self.final_layer(result)
+        return result
+
+    def reparameterize(self, mu: Tensor, logvar: Tensor) -> Tensor:
+        """
+        Reparameterization trick to sample from N(mu, var) from
+        N(0,1).
+        :param mu: (Tensor) Mean of the latent Gaussian [B x D]
+        :param logvar: (Tensor) Standard deviation of the latent Gaussian [B x D]
+        :return: (Tensor) [B x D]
+        """
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return eps * std + mu
+
+    def forward(self, input: Tensor, **kwargs) -> List[Tensor]:
+        mu, log_var = self.encode(input)
+        z = self.reparameterize(mu, log_var)
+        return  [self.decode(z), input, mu, log_var]
+
+    def loss_function(self,
+                      *args,
+                      **kwargs) -> dict:
+        """
+        Computes the VAE loss function.
+        KL(N(\mu, \sigma), N(0, 1)) = \log \frac{1}{\sigma} + \frac{\sigma^2 + \mu^2}{2} - \frac{1}{2}
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        recons = args[0]
+        input = args[1]
+        mu = args[2]
+        log_var = args[3]
+
+        kld_weight = kwargs['M_N'] # Account for the minibatch samples from the dataset
+        recons_loss =F.mse_loss(recons, input)
+
+
+        kld_loss = torch.mean(-0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp(), dim = 1), dim = 0)
+
+        loss = recons_loss + kld_weight * kld_loss
+        return {'loss': loss, 'Reconstruction_Loss':recons_loss.detach(), 'KLD':-kld_loss.detach()}
+
+    def sample(self,
+               num_samples:int,
+               current_device: int, **kwargs) -> Tensor:
+        """
+        Samples from the latent space and return the corresponding
+        image space map.
+        :param num_samples: (Int) Number of samples
+        :param current_device: (Int) Device to run the model
+        :return: (Tensor)
+        """
+        z = torch.randn(num_samples,
+                        self.latent_dim)
+
+        z = z.to(current_device)
+
+        samples = self.decode(z)
+        return samples
+
+    def generate(self, x: Tensor, **kwargs) -> Tensor:
+        """
+        Given an input image x, returns the reconstructed image
+        :param x: (Tensor) [B x C x H x W]
+        :return: (Tensor) [B x C x H x W]
+        """
+
+        return self.forward(x)[0]
+
+""" 
+vae = VAE(x_dim=784, hidden_dim1= 512, hidden_dim2=256, z_dim=20)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+vae.to(device)
+optimizer = optim.Adam(vae.parameters(), lr=3e-4)
+def loss_fn(recon_x, x, mu, var): # We use a single sample to reconstruct the loss
+    reconstruction_loss = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+    regul_loss = -0.5 * torch.sum(1 + var - mu.pow(2) - var.exp())
+    return reconstruction_loss + regul_loss
+def model_run(epochs):
+    for epoch in tqdm.tqdm(range(epochs)):
+        #Training 
+        vae.train()
+        train_loss = 0
+        for batch_idx, (data, _) in enumerate(train_loader):
+            data = data.to(device)
+            optimizer.zero_grad()
+            recon_batch, mu, var = vae(data)
+            loss = loss_fn(recon_batch, data, mu, var)
+            
+            loss.backward()
+            train_loss += loss.item()
+            optimizer.step()
+        if epoch % 10 == 0 :
+            print('Train loss : {:.4f}'.format(train_loss / len(train_loader.dataset)))
+        #Testing
+        vae.eval()
+        test_loss= 0
+        with torch.no_grad():
+            for data, _ in test_loader:
+                data = data.to(device)
+                recon, mu, var = vae(data)
+                
+                # sum up batch loss
+                test_loss += loss_fn(recon, data, mu, var).item()
+            
+        if epoch % 10 == 0 :
+            print('Test loss: {:.4f}'.format(test_loss / len(test_loader.dataset)))
+        if epoch in (0, 9, 79):
+            save_sample(epoch)
+             """
---
+++ common.py
@@ -0,0 +1,8 @@
+import torch
+import numpy as np
+import torch.nn as nn
+from torchview import draw_graph
+from torchvision.transforms import transforms as T
+import torchvision.transforms.functional as F
+from PIL import Image 
+import os
